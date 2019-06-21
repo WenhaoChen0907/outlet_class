6254821ab13012b5d8af0817d545b89d65b69cdb
hitheima <hitheima@163.com>
2019-06-21 10:42:07 +0800

    登录 - 参数化

--- scripts/test_login.py
+++ scripts/test_login.py
@@ -1,5 +1,10 @@
+from selenium.webdriver.common.by import By
+
+from base.base_analyze import analyze_data
 from base.base_driver import init_driver
 from page.page import Page
+
+import pytest
 
 
 class TestLogin:
@@ -8,16 +13,28 @@ class TestLogin:
         self.driver = init_driver()
         self.page = Page(self.driver)
 
-    def test_login(self):
+    @pytest.mark.parametrize("args", analyze_data("login_data", "test_login"))
+    def test_login(self, args):
+        # 获取数据
+        username = args["username"]
+        password = args["password"]
+        toast = args["toast"]
+
         # 首页 - 点击 我
         self.page.home.click_me()
         # 注册 - 点击 已有账号去登陆
         self.page.register.click_login()
         # 登陆 - 输入 账户
-        self.page.login.input_username("itheima_test")
+        self.page.login.input_username(username)
         # 登陆 - 输入 密码
-        self.page.login.input_password("itheima")
+        self.page.login.input_password(password)
         # 登陆 - 点击 登陆
         self.page.login.click_login()
-        # 断言："我"页面的用户名内容，是否和输入一致
-        assert self.page.me.get_username_text() == "itheima_test"
+
+        if toast is None:
+            # 断言："我"页面的用户名内容，是否和输入一致
+            assert self.page.me.get_username_text() == "itheima_test"
+        else:
+            # 断言：toast内容是否和预期一致
+            xpath = By.XPATH, "//*[@text='" + toast + "']"
+            assert self.page.login.find_element(xpath)
